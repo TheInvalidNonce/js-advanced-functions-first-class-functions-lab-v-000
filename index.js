20a46f8a2d6bea8b6d78b52676c245777c169f37
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-31 00:37:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ const returnLastTwoDrivers = function (arr) {
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (fare) {
+  return fare * 4
+}
